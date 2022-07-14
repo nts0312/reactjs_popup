0fe9f14c8c48c91afc0e586b0c40de440abfd5de
Nitin Singh <nitinsinghm111@gmail.com>
2022-07-14 19:59:47 +0530

    popup in reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [popup, setPop] = useState(false);
+  const handleClickOpen = () => {
+    setPop(!popup);
+  };
+
+  const closePopup = () => {
+    setPop(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClickOpen}>Open Popup</button>
+      <div>
+        {popup ? (
+          <div className="main">
+            <div className="popup">
+              <div className="popup-header">
+                <h1>popup</h1>
+                <h1 onClick={closePopup}>❌</h1>
+              </div>
+              <div>
+                <p>This is popup</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
